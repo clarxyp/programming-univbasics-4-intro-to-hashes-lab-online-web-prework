ccaeb48f4d15a337bedca91d6c1f3a8b90fb1a79
Clara Yip <clara.yip1@gmail.com>
2019-06-26 11:03:01 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,7 +21,7 @@ def id_generator
 end
 
 def my_hash_creator(key, value)
-  catch_phrase.new(greeting: "Eh, what's up, Doc?")
+  catch_phrase.new(:greeting, "Eh, what's up, Doc?")
 end
 
 def read_from_hash(hash, key)
